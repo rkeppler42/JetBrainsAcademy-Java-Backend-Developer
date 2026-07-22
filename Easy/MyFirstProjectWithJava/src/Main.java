ea8a388ce098424c2d6486aeb8f45a9ba0c84622
Roberto Keppler <rkeppler42@gmail.com>
2026-07-22 13:19:16 -0300

    feat: complete stage 2 income calculation

--- Easy/MyFirstProjectWithJava/src/Main.java
+++ Easy/MyFirstProjectWithJava/src/Main.java
@@ -1,11 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Prices:");
-        System.out.println("Bubblegum: $2");
-        System.out.println("Toffee: $0.2");
-        System.out.println("Ice cream: $5");
-        System.out.println("Milk chocolate: $4");
-        System.out.println("Doughnut: $2.5");
-        System.out.println("Pancake: $3.2");
+        System.out.println("Earned amount:");
+        System.out.println("Bubblegum: $202");
+        System.out.println("Toffee: $118");
+        System.out.println("Ice cream: $2250");
+        System.out.println("Milk chocolate: $1680");
+        System.out.println("Doughnut: $1075");
+        System.out.println("Pancake: $80");
+
+        double income = 202 + 118 + 2250 + 1680 + 1075 + 80;
+        System.out.println("Income: $" + income);
     }
 }
